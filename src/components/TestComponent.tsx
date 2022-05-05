b39d0b07646d948a62bae704796ef8d4cb373109
Marcin <marcinlacala@gmail.com>
2022-05-06 00:01:37 +0200

    front start adding  missing files

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./TestComponent.scss"
+export const TestComponent = () => (
+
+    <p>Siema eniu </p>
+
+)
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {TestComponent} from "./components/TestComponent";
+
 
 ReactDOM.render(
     <React.StrictMode>
         <App />
+        <TestComponent/>
     </React.StrictMode>,
     document.getElementById('root')
 );
