4634c0059ea7d11cd11d25f94b42bf88cc881d36
Marcin <marcinlacala@gmail.com>
2022-05-16 15:05:25 +0200

    fix import and directory structure

--- src/Views/HomeView.tsx
+++ src/Views/HomeView.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Home} from "../components/Home/Home";
-import {Footer} from "../components/Footer/Footer";
-import {Header} from "../components/Header/Header";
+import {Footer} from "../components/common/Footer/Footer";
+import {Header} from "../components/common/Header/Header";
 
 export const HomeView = () =>(
     <>
